5116f33b2b14869bf84933182e440c9f429f2cfa
Kimberly Owens <kturner2232@gmail.com>
2024-07-29 10:39:00 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import Dashboard from './components/Dashboard';
+
+const App = () => {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+};
+
+export default App;
